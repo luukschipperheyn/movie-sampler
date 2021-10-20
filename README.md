b7f199031da4b45595c81271d6a94cfbcc2034d1
Luuk <luuk@luuk.computer>
2021-10-20 02:36:38 +0200

    docs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # movie-sampler
 
-Samples sound effects from movies, based on [ Subtitles for the Deaf or Hard-of-Hearing ].
+Samples sound effects from movies, based on subtitle files for the deaf or hard-of-hearing.
 
 Download a movie with subtitles that have captions for noises using [square brackets] and run them through this script to generate a series of audio files trimmed to the duration of captioned noises.
 
@@ -26,6 +26,36 @@ yarn global add movie-sampler
 
 ## Usage
 
+### cli
+
 ```bash
-movie-sampler --video /path/to/video.mp4 --subs /path/to/subs.srt --out /output/directory
+Synopsis
+
+  $ movie-sampler --video /path/to/video/file.mp4 --subs
+  /path/to/subtitle/file.srt --out /path/to/output/directory/
+
+Options
+
+  --video file <required>   path to video file
+  --subs file <required>    path to subtitle (.srt) file
+  --out path <required>     path to output samples
+  --margin seconds          make longer samples. Adds [margin] seconds to start and to end of each
+                            sample.
+  --ffmpegPath path         path to ffmpeg executable
+  --help string             Print this usage guide.
+
+```
+
+### javascript module
+
+```javascript
+const movieSampler = require("movie-sampler");
+
+const options = {
+  video: "/path/to/video.mp4",
+  subs: "/path/to/subtitles.srt",
+  out: "/path/to/output/directory/",
+};
+
+await movieSampler(options);
 ```
--- bin/index.js
+++ bin/index.js
@@ -1,62 +1,82 @@
 #!/usr/bin/env node
 
-const commandLineArgs = require('command-line-args')
-const commandLineUsage = require('command-line-usage')
-const process = require('process')
-const movieSampler = require('../lib/movie-sampler')
+const commandLineArgs = require("command-line-args");
+const commandLineUsage = require("command-line-usage");
+const process = require("process");
+const movieSampler = require("../lib/movie-sampler");
 
 const sections = [
-    {
-        header: 'movie-sampler',
-        content: 'samples sound effects from movies, based on [ Subtitles for the Deaf or Hard-of-Hearing ]'
-    },
-    {
-        header: 'Options',
-        optionList: [
-            {
-                name: 'video',
-                typeLabel: '{underline file}',
-                description: 'path to video file'
-            },
-            {
-                name: 'subs',
-                typeLabel: '{underline file}',
-                description: 'path to subtitle (.srt) file'
-            },
-            {
-                name: 'out',
-                typeLabel: '{underline path}',
-                description: 'path to output samples'
-            },
-            {
-                name: 'help',
-                description: 'Print this usage guide.'
-            }
-        ]
-    }
-]
+  {
+    header: "movie-sampler",
+    content:
+      "samples sound effects from movies, based on subtitle files for the deaf or hard-of-hearing, containing noise captions using square brackets",
+  },
+  {
+    header: "Synopsis",
+    content: [
+      "$ movie-sampler {bold --video} {underline /path/to/video/file.mp4} {bold --subs} {underline /path/to/subtitle/file.srt} {bold --out} {underline /path/to/output/directory/}",
+    ],
+  },
+  {
+    header: "Options",
+    optionList: [
+      {
+        name: "video",
+        typeLabel: "{underline file} <required>",
+        description: "path to video file",
+      },
+      {
+        name: "subs",
+        typeLabel: "{underline file} <required>",
+        description: "path to subtitle (.srt) file",
+      },
+      {
+        name: "out",
+        typeLabel: "{underline path} <required>",
+        description: "path to output samples",
+      },
+      {
+        name: "margin",
+        typeLabel: "{underline seconds}",
+        description:
+          "make longer samples. Adds [margin] seconds to start and to end of each sample.",
+      },
+      {
+        name: "ffmpegPath",
+        typeLabel: "{underline path}",
+        description: "path to ffmpeg executable",
+      },
+      {
+        name: "help",
+        description: "Print this usage guide.",
+      },
+    ],
+  },
+];
 
 const optionDefinitions = [
-    { name: 'video', type: String },
-    { name: 'subs', type: String },
-    { name: 'out', type: String },
-    { name: 'help', alias: 'h', type: Boolean }
-]
-const options = commandLineArgs(optionDefinitions)
+  { name: "video", type: String },
+  { name: "subs", type: String },
+  { name: "out", type: String },
+  { name: "margin", type: Number },
+  { name: "ffmpegPath", type: String },
+  { name: "help", alias: "h", type: Boolean },
+];
+const options = commandLineArgs(optionDefinitions);
 
 if (options.help) {
-    const usage = commandLineUsage(sections)
-    console.log(usage)
-    process.exit(0)
+  const usage = commandLineUsage(sections);
+  console.log(usage);
+  process.exit(0);
 }
-console.log('')
+console.log("");
 movieSampler(options)
-    .then(() => {
-        console.log('done\n');
-        process.exit(0);
-    })
-    .catch(e => {
-        console.error(e.message);
-        console.log('run "movie-sampler --help" for instructions\n');
-        process.exit(1);
-    })
+  .then(() => {
+    console.log("done\n");
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e.message);
+    console.log('run "movie-sampler --help" for instructions\n');
+    process.exit(1);
+  });
